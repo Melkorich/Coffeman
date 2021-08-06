7fa720c3179759884ac2098152ad3f83813c1cb0
Melkorich <Kirill.Lavrov92@gmail.com>
2021-08-06 22:57:22 +0300

    bestseller working

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
 $(function(){
+
+  $('.bestsellers-slider').slick({
+
+    // prevArrow: '<button type="button" class="slick-prev"><img src="images/bestsellers/arrow-left.svg" alt="prev"></button>',
+    // nextArrow: '<button type="button" class="slick-prev"><img src="images/bestsellers/arrow-left.svg" alt="prev"></button>',
+  });
  
 });
