a6e9b204a9609270aff4d62170493329b8021a77
Melkorich <Kirill.Lavrov92@gmail.com>
2021-08-07 18:51:27 +0300

    slider finished

--- app/js/main.js
+++ app/js/main.js
@@ -2,8 +2,13 @@ $(function(){
 
   $('.bestsellers-slider').slick({
 
-    // prevArrow: '<button type="button" class="slick-prev"><img src="images/bestsellers/arrow-left.svg" alt="prev"></button>',
-    // nextArrow: '<button type="button" class="slick-prev"><img src="images/bestsellers/arrow-left.svg" alt="prev"></button>',
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/bestsellers/arrow-left.svg" alt="prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/bestsellers/arrow-right.svg" alt="next"></button>',
+
+    slidesToShow: 3,
+    centerMode: true,
+    centerPadding: '0px',
+    focusOnSelect: true,
   });
  
 });
