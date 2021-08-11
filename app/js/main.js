21250a6cf50c8821f5776730b6f61accc4cad657
Melkorich <Kirill.Lavrov92@gmail.com>
2021-08-12 01:23:49 +0300

    fixes

--- app/js/main.js
+++ app/js/main.js
@@ -1,31 +1,22 @@
-$(function(){
-
-  // $("a[href^='#']").click(function(){
-  //   let target = $(this).attr("href");
-  //   $("html, body").animate({
-  //     scrollTop: $(target).offset().top-0
-  //     }, 1000);
-  // });
-
-  
+$(document).ready(function() {
 
   $("a[href^='#']").click(function(){
     var _href = $(this).attr("href");
     $("html").animate({scrollTop: $(_href).offset().top+"px"}, 2000);
     return false;
   });
-
+  
   $('.menu__btn, menu').on('click', function() {
     $('.menu__list').toggleClass('menu__list--active');
     $('.menu__btn').toggleClass('menu__btn--active');
     // $('body').toggleClass('lock');
   });
-
+  
   $('.reviews-slider').slick({
     arrows: false,
     dots: true,
   });
-
+      
   $('.bestsellers-slider').slick({
 
     prevArrow: '<button type="button" class="slick-prev"><img src="images/bestsellers/arrow-left.svg" alt="prev"></button>',
@@ -49,12 +40,13 @@ $(function(){
         breakpoint: 768,
         settings: {
           slidesToShow: 1,
+          arrows: false,
+          dots: true,
         }
       },
     ]
   });
-  
-
+    
   $(".rate").rateYo({
     starWidth: "30px",
     normalFill: "#e5e5e5",
@@ -63,8 +55,7 @@ $(function(){
     readOnly: true,
     'starSvg': '<svg xmlns="http://www.w3.org/2000/svg"><path d="M14.2821 0L17.5864 10.1699L28.2797 10.1699L19.6287 16.4552L22.933 26.625L14.2821 20.3397L5.63106 26.625L8.93544 16.4552L0.28445 10.1699L10.9777 10.1699L14.2821 0Z"></svg>',
   });
-
+  
   new WOW().init();
-
   
 });
