1708104379a7b01f2296fd3a9060f4a473b753a9
Melkorich <Kirill.Lavrov92@gmail.com>
2021-08-12 22:01:14 +0300

    fixes

--- app/js/main.js
+++ app/js/main.js
@@ -9,7 +9,6 @@ $(document).ready(function() {
   $('.menu__btn, menu').on('click', function() {
     $('.menu__list').toggleClass('menu__list--active');
     $('.menu__btn').toggleClass('menu__btn--active');
-    // $('body').toggleClass('lock');
   });
   
   $('.reviews-slider').slick({
@@ -24,7 +23,7 @@ $(document).ready(function() {
 
     slidesToShow: 3,
     centerMode: true,
-    centerPadding: '0px',
+    centerPadding: 'px',
     focusOnSelect: true,
 
     responsive: [
@@ -42,6 +41,7 @@ $(document).ready(function() {
           slidesToShow: 1,
           arrows: false,
           dots: true,
+          swipe: true,
         }
       },
     ]
@@ -56,6 +56,14 @@ $(document).ready(function() {
     'starSvg': '<svg xmlns="http://www.w3.org/2000/svg"><path d="M14.2821 0L17.5864 10.1699L28.2797 10.1699L19.6287 16.4552L22.933 26.625L14.2821 20.3397L5.63106 26.625L8.93544 16.4552L0.28445 10.1699L10.9777 10.1699L14.2821 0Z"></svg>',
   });
   
+  // let wow = new WOW(
+  //   {
+  //     mobile: false
+  //   }
+  // );
+
   new WOW().init();
-  
+
+
 });
+
